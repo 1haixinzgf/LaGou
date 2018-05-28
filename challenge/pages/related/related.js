5629d096f84c9ff1e4f781a34e6665c6b12dba93
unknown <2725965600@qq.com>
2018-05-28 23:53:56 +0800

    update

---
+++ challenge/pages/related/related.js
@@ -0,0 +1,71 @@
+// pages/related/related.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  salary:["默认","2K以下","2K-5k","5k-10k","10k-15k","15k-25k","25k-50k","50k以上"],
+  publishtime: ["默认","今天发布","三天内","一周内","两周内"],
+  experience: ["应届毕业生","3年以下","3-5年","5-10年","10年以上"],
+  education: ["大专","本科","硕士","博士","不要求"],
+  mode: ["全职","兼职","实习"]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
