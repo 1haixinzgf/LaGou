b8b119261bf1a27b0a05abd68032e87195ffb82e
unknown <2725965600@qq.com>
2018-05-29 15:56:48 +0800

    部分bug修复

--- challenge/pages/related/related.js
+++ challenge/pages/related/related.js
@@ -6,13 +6,89 @@ Page({
    */
   data: {
   salary:["默认","2K以下","2K-5k","5k-10k","10k-15k","15k-25k","25k-50k","50k以上"],
+  salaryShowIndex: '',
   publishtime: ["默认","今天发布","三天内","一周内","两周内"],
+  publishtimeShowIndex:'',
   experience: ["应届毕业生","3年以下","3-5年","5-10年","10年以上"],
+  experienceShowIndex:'',
   education: ["大专","本科","硕士","博士","不要求"],
-  mode: ["全职","兼职","实习"]
+  educationShowIndex:'',
+  mode: ["全职","兼职","实习"],
+  modeShowIndex:'',
+  financing: ["未融资","天使轮","A轮","B轮","C轮","D轮及以上","上市公司","不需要融资"],
+  financingShowIndex:'',
+  field: ["不限","移动互联网","电子商务","社交网络","O2O","企业服务","游戏","教育"],
+  fieldShowIndex:''
+
 
   },
+  select(e) {
+    // console.log(e);
+    // const currentindex = e.currentTarget.dataset;
+    // console.log(value);
+    
 
+  },
+  selectsalary(e) {
+    // console.log(e);
+    const index = e.currentTarget.dataset.index;
+    let  salaryShowIndex = this.data.salaryShowIndex;
+    salaryShowIndex = index;
+    console.log(salaryShowIndex);
+    this.setData({
+      salaryShowIndex
+    })
+  },
+  selectpublishtime(e) {
+    // console.log(e);
+    const index = e.currentTarget.dataset.index;
+    let  publishtimeShowIndex = this.data.publishtimeShowIndex;
+    publishtimeShowIndex = index;
+    console.log(publishtimeShowIndex);
+    this.setData({
+      publishtimeShowIndex
+    })
+  },
+  selecteducation(e) {
+    // console.log(e);
+    const index = e.currentTarget.dataset.index;
+    let  experienceShowIndex = this.data.experienceShowIndex;
+    experienceShowIndex = index;
+    console.log(experienceShowIndex);
+    this.setData({
+      experienceShowIndex
+    })
+  },
+  selectmode(e) {
+    // console.log(e);
+    const index = e.currentTarget.dataset.index;
+    let  educationShowIndex = this.data.educationShowIndex;
+    educationShowIndex = index;
+    console.log(educationShowIndex);
+    this.setData({
+      educationShowIndex
+    })
+  },
+  selectfinancing(e) {
+    // console.log(e);
+    const index = e.currentTarget.dataset.index;
+    let  salaryShowIndex = this.data.financingShowIndex;
+    financingShowIndex = index;
+    console.log(financingShowIndex);
+    this.setData({
+      financingShowIndex
+    })
+  },
+  selectfield(e) {
+    // console.log(e);
+    const index = e.currentTarget.dataset.index;
+    let  fieldShowIndex = this.data.fieldShowIndex;
+    fieldShowIndex = index;
+    console.log(fieldShowIndex);
+    this.setData({
+      fieldShowIndex
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
